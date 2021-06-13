a054a8c6eaac8f974cfc297cede5120d6ae4720e
Oliver Sõro <oliver.s6ro@gmail.com>
2021-06-13 15:17:22 +0300

    Tweak Sign in to look a bit nicer.

--- app/lib/login.dart
+++ app/lib/login.dart
@@ -18,61 +18,70 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Form(
-        key: _formKey,
-        child: Consumer<AppModel>(
-          builder: (BuildContext ctx, AppModel model, Widget? w) {
-            return Column(
-              children: <Widget>[
-                TextFormField(
-                  autocorrect: false,
-                  decoration: InputDecoration(labelText: 'Email'),
-                  keyboardType: TextInputType.emailAddress,
-                  textCapitalization: TextCapitalization.none,
-                  validator: (v) =>
-                      v != null && v.isEmpty ? 'Email is required' : null,
-                  onSaved: (v) => model.credentials.email = v ?? '',
-                ),
-                TextFormField(
-                  autocorrect: false,
-                  decoration: InputDecoration(labelText: 'Password'),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  textCapitalization: TextCapitalization.none,
-                  validator: (v) =>
-                      v != null && v.isEmpty ? 'Password is required' : null,
-                  onSaved: (v) => model.credentials.pass = v ?? '',
-                ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      try {
-                        var ctrl = Scaffold.of(context).showBottomSheet(
-                            (context) => Text('Sending credentials ...'));
-                        Jwt jwt = await Api.login(model.credentials);
-                        Claims c = await Api.claims(jwt);
-
-                        print('$jwt');
-                        print('$c');
-
-                        model.user = User(jwt, c);
-                        ctrl.close();
-                      } catch (e) {
-                        Scaffold.of(context).showBottomSheet(
-                            (context) => Text((e as AppError).message));
-                      }
-                    }
-                  },
-                  child: Text('Submit'),
-                ),
-              ],
-            );
-          },
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).primaryColor),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            padding: const EdgeInsets.all(16),
+            width: 480,
+            child: Form(
+              key: _formKey,
+              child: Consumer<AppModel>(
+                builder: (BuildContext ctx, AppModel model, Widget? w) {
+                  return Column(
+                    children: <Widget>[
+                      TextFormField(
+                        autocorrect: false,
+                        decoration: InputDecoration(labelText: 'Email'),
+                        keyboardType: TextInputType.emailAddress,
+                        textCapitalization: TextCapitalization.none,
+                        validator: (v) =>
+                            v != null && v.isEmpty ? 'Email is required' : null,
+                        onSaved: (v) => model.credentials.email = v ?? '',
+                      ),
+                      TextFormField(
+                        autocorrect: false,
+                        decoration: InputDecoration(labelText: 'Password'),
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        textCapitalization: TextCapitalization.none,
+                        validator: (v) => v != null && v.isEmpty
+                            ? 'Password is required'
+                            : null,
+                        onSaved: (v) => model.credentials.pass = v ?? '',
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
+                            try {
+                              var ctrl = Scaffold.of(context).showBottomSheet(
+                                  (context) => Text('Sending credentials ...'));
+                              Jwt jwt = await Api.login(model.credentials);
+                              Claims c = await Api.claims(jwt);
+                              model.user = User(jwt, c);
+                              ctrl.close();
+                            } catch (e) {
+                              Scaffold.of(context).showBottomSheet(
+                                  (context) => Text((e as AppError).message));
+                            }
+                          }
+                        },
+                        child: Text('Sign in'),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
