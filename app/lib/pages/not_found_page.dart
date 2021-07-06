3a7d1a43eb8720a9630ecc7fd8bd752aec2372d0
Oliver Sõro <oliver.s6ro@gmail.com>
2021-07-06 17:12:27 +0300

    A bit nicer NotFoundPage implementation.

--- app/lib/pages/not_found_page.dart
+++ app/lib/pages/not_found_page.dart
@@ -8,8 +8,27 @@ class NotFoundPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return APage(
       key: key,
-      body: Container(
-        child: Text('Not Found'),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2,
+          child: Column(
+            children: [
+              Center(
+                child: Text(
+                  'Page Not Found',
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+              ),
+              SizedBox(height: 25),
+              Center(
+                child: Text(
+                  'Requested URL does not point to a valid page.',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
